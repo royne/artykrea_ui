2af998cd00e979ead0b31cdcbdb4a342ddf841ec
royne <royne.chaves@gmail.com>
2020-10-27 11:36:09 -0500

    add view list

--- src/Components/admin/Home.js
+++ src/Components/admin/Home.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../styles/admin.css'
 import Form from './Form';
+import List from './List';
 import SideNav from './SideNav';
 
 const Home = () => {
+  const [activeComponent, setActiveComponent] = useState('Form')
+
+  const setComponent = name => setActiveComponent(name)
+
   return (
     <section className="admin">
-      <SideNav />
+      <SideNav setComponent={setComponent} />
       <div className="admin_content">
-        <Form />
+        { activeComponent === 'Form' ? <Form /> : <List /> }
       </div>
     </section> 
    );
--- src/Components/admin/SideNav.js
+++ src/Components/admin/SideNav.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import logo from '../../assets/images/logo_pink.png'
 
-const SideNav = () => {
+const SideNav = ({setComponent}) => {
+  const getComponent = e => {
+    let name = e.target.getAttribute('name')
+    setComponent(name)
+  }
+
   return ( 
     <div className="side_nav">
       <img src={logo} alt="artykrea"/>
       <div className="side_nav_menu">
         <ul>
-          <li>Nuevo Producto</li>
-          <li>Inventario</li>
+          <li onClick={getComponent} name="Form">Nuevo Producto</li>
+          <li onClick={getComponent} name="List">Inventario</li>
         </ul>
       </div>
     </div>
